c98a5bc34e0000ba8a4a75a5b583bc4cb60beee6
karnjot Singh <karnjot.singh@smu.ca>
2021-11-20 12:43:50 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MCDA_Test_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
